a3a2dc4d9e840300783dc3f86aa0fb90c36e4bb1
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2022-11-15 16:03:54 +0800

    introduce `anyhow` to reduce example
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- integer-add/host/src/main.rs
+++ integer-add/host/src/main.rs
@@ -1,22 +1,19 @@
+use anyhow::Error;
 use wasmedge_sdk::{
     config::{CommonConfigOptions, ConfigBuilder, HostRegistrationConfigOptions},
     Vm,
 };
 
-fn main() {
+fn main() -> Result<(), Error> {
     let config = ConfigBuilder::new(CommonConfigOptions::default())
         .with_host_registration_config(HostRegistrationConfigOptions::default().wasi(true))
-        .build()
-        .expect("fail to create config");
-    let mut vm = Vm::new(Some(config)).expect("fail to create vm");
-    vm = vm
-        .register_module_from_file("module", "target/wasm32-wasi/release/module.wasm")
-        .expect("fail to register module");
-    vm = vm
-        .register_module_from_file("app", "target/wasm32-wasi/release/app.wasm")
-        .expect("fail to register app");
-    let result = vm
-        .run_func(Some("app"), "start", None)
-        .expect("fail to run");
+        .build()?;
+    let vm = Vm::new(Some(config))?
+        .register_module_from_file("module", "target/wasm32-wasi/release/module.wasm")?
+        .register_module_from_file("app", "target/wasm32-wasi/release/app.wasm")?;
+
+    let result = vm.run_func(Some("app"), "start", None)?;
     println!("result: {}", result[0].to_i32());
+
+    Ok(())
 }
